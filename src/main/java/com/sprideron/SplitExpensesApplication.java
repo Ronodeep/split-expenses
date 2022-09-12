6ea5605f60665ee624a7795d512b03c1f3d45d21
Ronodeep <ronodeepbhattacharya@gmail.com>
2022-09-12 12:26:51 +0530

    Intial Project setup

---
+++ src/main/java/com/sprideron/SplitExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.sprideron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitExpensesApplication.class, args);
+	}
+
+}
